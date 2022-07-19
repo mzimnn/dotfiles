ddf85b7a76d9537426ebef9937d533f20d6f2a6a
Manuel Zimmermann <m@zimnn.de>
2022-07-19 21:35:23 +0200

    emacs: always use relative paths for file links in Org mode
    All my Org files are stored in two different Git repositories. Both of
    them are in the same directory. If relative paths are used then I can
    freely move these two repositories as long as they stay in the same
    directory without breaking any links.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -41,6 +41,9 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)" "HOLD(h)" "WAIT(w)" "|" "DONE(d)" )))
 
+;; use relative paths for links
+(setq org-link-file-path-type 'relative)
+
 ;; keybindings to invoke Org features
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c l") 'org-store-link)
