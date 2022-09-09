8863d11e142fb730631e84f95069d11b13728150
Manuel Zimmermann <m@zimnn.de>
2022-09-09 21:07:53 +0200

    emacs: allow replacing one host with another
    For some websites exist more lightweight frontends which use less
    JavaScript. This comes handy when viewing them in EWW, since most of the
    time they are rendered a lot better.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -144,6 +144,33 @@
 ;; do not display images
 (setq shr-inhibit-images t)
 
+;; use lightweight frontends which EWW can render better
+(setq mz/url-replace-host-map '(("reddit.com" . "libredd.it")
+                                ("old.reddit.com" . "libredd.it")
+                                ("www.reddit.com" . "libredd.it")))
+
+(defun mz/url-replace-host (url)
+  "Replace host of URL according to `mz/url-replace-host-map'."
+  (let* ((parsed-url (url-generic-parse-url url))
+         (host (url-host parsed-url))
+         (host-replacement
+          (alist-get host mz/url-replace-host-map nil nil #'string-equal)))
+    (when host-replacement
+      (setf (url-host parsed-url) host-replacement))
+    (url-recreate-url parsed-url)))
+
+;; ensure host is correctly displayed while loading the page
+(advice-add 'eww--dwim-expand-url :filter-return #'mz/url-replace-host)
+;; ensure redirects are considered
+(advice-add 'url-retrieve-internal :filter-args
+            (lambda (args)
+              (let ((url (mz/url-replace-host (car args)))
+                    (status (car (caddr args))))
+                (when (plist-get status :redirect)
+                  (plist-put status :redirect url))
+                (cons url (cdr args))))
+            '((name . "mz/url-replace-host")))
+
 ;; allow repeating commands more easily
 (repeat-mode)
 
