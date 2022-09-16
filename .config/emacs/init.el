c60a7c2792f7f659e84c7cccc4a192915d7d61d1
Manuel Zimmermann <m@zimnn.de>
2022-09-16 23:04:38 +0200

    emacs: simplify installation of packages

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,21 +4,13 @@
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(defun mz/package-install-if-missing (wanted-pkgs)
-  "Install packages defined in WANTED-PKGS if they are missing."
-  (let ((missing-pkgs ()))
-    (dolist (wanted-pkg wanted-pkgs)
-      (unless (package-installed-p wanted-pkg)
-        (setq missing-pkgs (cons wanted-pkg missing-pkgs))))
-    (when (consp missing-pkgs)
-      (package-refresh-contents)
-      (dolist (missing-pkg missing-pkgs)
-        (package-install missing-pkg)))))
-
 ;; install packages
 (setq package-selected-packages
       '(diff-hl ledger-mode magit solarized-theme))
-(mz/package-install-if-missing package-selected-packages)
+
+(unless (seq-every-p #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (package-install-selected-packages))
 
 ;; use local directory to modularize this init file
 (add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
