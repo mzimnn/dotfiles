ba37087e328d2dc900c2aa208e0a01861a4c34eb
Manuel Zimmermann <m@zimnn.de>
2023-01-14 23:22:28 +0100

    emacs: do not explicitly initialize package.el
    Since Emacs 27 packages are activated before loading the init file. As a
    result, I don't have to call `package-initialize` in my own init file
    anymore.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,8 +1,6 @@
 ;; set up package.el to work with MELPA
-(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
 ;; install packages
 (setq package-selected-packages
