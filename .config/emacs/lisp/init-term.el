0607a7877ca7c904a2e0f76b12b30643072749f3
Manuel Zimmermann <m@zimnn.de>
2023-05-24 20:20:00 +0200

    emacs: kill term buffer after exit
    When the shell in the term buffer is exited, I don't need the buffer
    anymore. Kill it.

--- .config/emacs/lisp/init-term.el
+++ .config/emacs/lisp/init-term.el
@@ -10,11 +10,20 @@ the existing term buffer."
         (display-buffer buffer)
       (ansi-term "bash"))))
 
+(defun mz/term-handle-exit (process-name msg)
+  "Kill the current buffer.
+
+It can be used to advice `term-handle-exit'."
+  (kill-buffer))
+
 (with-eval-after-load 'term
   (define-key term-raw-map (kbd "M-o") nil)
   (define-key term-raw-map (kbd "M-x") nil))
 
 ;; make URLs and email addresses clickable
 (add-hook 'term-mode-hook #'goto-address-mode)
+
+;; kill term buffer after exit
+(advice-add #'term-handle-exit :after #'mz/term-handle-exit)
 
 (provide 'init-term)
