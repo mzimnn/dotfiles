52ef2279e9e6b598077fba0a34fdd4534b094fcf
Manuel Zimmermann <m@zimnn.de>
2024-02-18 17:21:27 +0100

    emacs: adjust sorting in init-editing.el
    This commit sorts the `setq-default` calls in alphabetical order and
    groups them together. This makes this file more readable.

--- .config/emacs/lisp/init-editing.el
+++ .config/emacs/lisp/init-editing.el
@@ -1,20 +1,20 @@
 ;; specify when to wrap lines
 (setq-default fill-column 80)
 
+;; by default use spaces for indentation
+(setq-default indent-tabs-mode nil)
+
+;; specify width of TAB character
+(setq-default tab-width 4)
+
 ;; let a single space end a sentence
 (setq sentence-end-double-space nil)
 
 ;; replace selection with typed text
 (delete-selection-mode)
 
-;; specify width of TAB character
-(setq-default tab-width 4)
-
 ;; guess indentation settings
 (setq dtrt-indent-verbosity 0)
 (dtrt-indent-global-mode)
-
-;; by default use spaces for indentation
-(setq-default indent-tabs-mode nil)
 
 (provide 'init-editing)
